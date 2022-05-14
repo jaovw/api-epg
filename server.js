ec936efc873afbc38d4a1d8bd09c78557d7babf1
jaovw <joojvw@gmail.com>
2022-05-14 15:49:32 -0300

    Utilizando ejs para renderizar os dados

--- server.js
+++ server.js
@@ -5,5 +5,6 @@ const router = require('./router')
 const port = 3333
 
 app.use(router)
+app.set('view engine', 'ejs')
 
-app.listen(port, () => console.info(`Rodando em http://localhost:${port}`))
+app.listen(port, () => console.info(`Rodando na porta:${port} -- Acesse http://34.201.36.236:3333/`))
