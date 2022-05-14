8a1174d6927a13d1e4ac7cab273dcbc45ce1e2af
jaovw <joojvw@gmail.com>
2022-05-14 15:11:06 -0300

    Aplicando Router

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const app = express()
+const router = require('./router')
 
 const port = 3333
 
+app.use(router)
 
-
-app.listen(port)
+app.listen(port, () => console.info(`Rodando em http://localhost:${port}`))
