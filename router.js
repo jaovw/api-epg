c1bb50dd0dcc589574b4f1f5dbf20ab3bad7777f
jaovw <joojvw@gmail.com>
2022-05-15 15:37:55 -0300

    Mudando forma de param data axios

--- router.js
+++ router.js
@@ -2,17 +2,9 @@ const express = require('express')
 const router = express.Router()
 const {getDadosApi} = require('./src/controller/apiController')
 
-const dataParametro = new Date().toLocaleDateString()
-
-const [dia,mes,ano] = dataParametro.split('/')
-
 router.get('/', async (req, res) =>{
 
     let dataAbreviada = Math.floor(new Date().getTime() / 1000)
-
-    
-
-    let diaParametro = `${ano}-${mes}-${dia}`
 
     const dataFront = new Date().toLocaleString('pt-br', { hour12: false, timeZone: 'UTC',dateStyle: 'short'})
 
--- src/controller/apiController.js
+++ src/controller/apiController.js
@@ -1,14 +1,13 @@
 const axios = require('axios')
 
-const dataParametro = new Date().toLocaleDateString()
-
-const [dia,mes,ano] = dataParametro.split('/')
-
-const diaParametro = `${ano}-${mes}-${dia}`
-
 async function getDadosApi() {
+    const dataParametro = new Date().toLocaleDateString()
 
-    const resultado = await axios(`https://epg-api.video.globo.com/programmes/1337?date=`+diaParametro).catch(e => console.log(e))
+    const [dia,mes,ano] = dataParametro.split('/')
+
+    const diaParametro = `${ano}-${mes}-${dia}`
+
+    const resultado = await axios('https://epg-api.video.globo.com/programmes/1337?date='+diaParametro).catch(e => console.log(e))
     return resultado.data.programme.entries
 }
 
