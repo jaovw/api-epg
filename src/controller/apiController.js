fe164de76cc3f178cda6348b29ed5507590bb826
joao.victor <joao.victor@superfestval.com.br>
2022-05-16 08:29:14 -0300

    Consideracao final p formato data server

--- src/controller/apiController.js
+++ src/controller/apiController.js
@@ -6,7 +6,7 @@ async function getDadosApi() {
     let [dia,mes,ano] = dataParametro.split('/')
 
     let diaParametro = `${ano}-${mes}-${dia}`
-
+                                                            //  LEVANDO EM CONSIDERAÇÃO O SERVIDOR COM FORMATO MM/DD/YYYY (HEROKU E VM AWS)
     const resultado = await axios(`https://epg-api.video.globo.com/programmes/1337?date=${ano}-${dia}-${mes}`).catch(e => console.log(e))
 
     return resultado.data.programme.entries
